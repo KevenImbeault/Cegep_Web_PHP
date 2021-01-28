68ac3a6523710f383289b69097bc7f3c429ca46b
Keven Imbeault <imbk25099802@collegealma.ca>
2021-01-28 15:57:03 -0500

    Modified non-connected navbar

--- index.php
+++ index.php
@@ -10,31 +10,12 @@
 ?>
 
 <!-- Contenu -->
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Keven Imbeault</title>
-
-    <link rel="stylesheet" href="./css/style.css" />
-
-
-
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-</head>
 <body>
-<nav id="homeNav">
-    <a href="./about.html">À propos</a>
-    <a href="">Compétences</a>
-    <a href="">Projets</a>
-</nav>
 <div id="home">
-    <h1>Keven Imbeault</h1>
+    <h1>Magasin du coin</h1>
     <hr />
 </div>
 </body>
-<script src="./fonctions.js"></script>
 </html>
 
 <!-- Pied de page -->
