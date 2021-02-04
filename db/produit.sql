48e44ec75bbed4a91c10333f62efe40ef3720cc6
Keven Imbeault <imbk25099802@collegealma.ca>
2021-02-04 12:50:25 -0500

    Added sql file and images

---
+++ db/produit.sql
@@ -0,0 +1,18 @@
+insert into produit_fr (nomProduit, categorie, fournisseur, quantite, format, prix, description) values ('Jeu de cartes', '1','BlaBla', 75, 'aucun', 3.99, 'Jeu de cartes en plastique');
+insert into produit_fr (nomProduit, categorie, fournisseur, quantite, format, prix, description) values ('Tablette de feuille', '2','BlaBla', 200, 'lettre (8.5 x 11)', 1.59, 'Tablette de feuilles blanches lignées');
+insert into produit_fr (nomProduit, categorie, fournisseur, quantite, format, prix, description) values ('Serviette de plage', '3','BlaBla', 20, 'aucun', 12.00, 'Serviette de plage avec différent motif');
+insert into produit_fr (nomProduit, categorie, fournisseur, quantite, format, prix, description) values ('Ballon', '1','BlaBla', 30, 'aucun', 4.50, 'Ballon en platique de différentes couleurs');
+insert into produit_fr (nomProduit, categorie, fournisseur, quantite, format, prix, description) values ('chaise de plage', '1','BlaBla', 75, 'aucun', 3.99, 'Jeu de cartes en plastique');
+insert into produit_fr (nomProduit, categorie, fournisseur, quantite, format, prix, description) values ('Savon', '1','BlaBla', 40, 'aucun', 2.50, 'Savon fait à la main par un artisan de la région.  Différentes fragrances sont disponibles');
+insert into produit_fr (nomProduit, categorie, fournisseur, quantite, format, prix, description) values ('Tasse', '1','BlaBla', 75, 'aucun', 6.99, 'Tasse avec un logo souvenir de la région');
+insert into produit_fr (nomProduit, categorie, fournisseur, quantite, format, prix, description) values ('Chandail', '1','BlaBla', 75, 'aucun', 14.99, 'Chandail avec un logo souvenir de la région');
+insert into produit_fr (nomProduit, categorie, fournisseur, quantite, format, prix, description) values ('Porte clés', '1','BlaBla', 75, 'aucun', 3.99, 'Jeu de cartes en plastique');
+insert into produit_fr (nomProduit, categorie, fournisseur, quantite, format, prix, description) values ('Chandelle', '1','BlaBla', 75, 'aucun', 3.99, 'Jeu de cartes en plastique');
+insert into produit_fr (nomProduit, categorie, fournisseur, quantite, format, prix, description) values ('Crayon', '1','BlaBla', 75, 'aucun', 3.99, 'Jeu de cartes en plastique');
+insert into produit_fr (nomProduit, categorie, fournisseur, quantite, format, prix, description) values ('Confiture', '1','BlaBla', 75, 'aucun', 3.99, 'Jeu de cartes en plastique');
+insert into produit_fr (nomProduit, categorie, fournisseur, quantite, format, prix, description) values ('Billes', '1','BlaBla', 75, 'aucun', 3.99, 'Jeu de cartes en plastique');
+insert into produit_fr (nomProduit, categorie, fournisseur, quantite, format, prix, description) values ('Jouets de plage', '1','BlaBla', 75, 'aucun', 3.99, 'Cet ensemble de plage comprend une chaudière, trois pelles et un tamis');
+insert into produit_fr (nomProduit, categorie, fournisseur, quantite, format, prix, description) values ('Pansement', '1','BlaBla', 12, 'aucun', 5.00, 'La boite contient 50 pansements de différentes grandeurs');
+
+
+
